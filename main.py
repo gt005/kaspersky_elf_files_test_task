77dabef42e9f350bccb8158e4228a388ca8059a7
Karim <karimhamid@yandex.ru>
2023-04-21 00:27:50 +0300

    check sanitizers function made

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def check_sanitizers_in_elf_file(file_path: str) -> dict[str: bool]:
+import argparse
+import subprocess
+import sys
+
+
+def check_sanitizers_in_elf_file(file_path: str) -> dict[str, bool]:
     """
     Check if the ELF file contains sanitizers:
     AddressSanitizer, ThreadSanitizer, MemorySanitizer.
@@ -7,11 +12,30 @@ def check_sanitizers_in_elf_file(file_path: str) -> dict[str: bool]:
     :return: dictionary with name of sanitizer as key and bool value
              of its presence
     """
-    pass
+    try:
+        output = subprocess.check_output(
+            ['readelf', '-s', file_path],
+            universal_newlines=True
+        )
+    except FileNotFoundError:
+        print(
+            'Make sure, that readelf is installed on your system and added to PATH'
+        )
+        sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f'Error while executing readelf: {e}')
+        sys.exit(1)
+
+    return {
+        'AddressSanitizer': '__asan_init' in output,
+        'ThreadSanitizer': '__tsan_init' in output,
+        'MemorySanitizer': '__msan_init' in output,
+    }
 
 
 def main():
-    pass
+    file_path = input('Specify the path to the required ELF file: ')
+    print(check_sanitizers_in_elf_file(file_path))
 
 
 if __name__ == '__main__':

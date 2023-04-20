f073699f5b3eadbfadc56ff54540189284b2067c
Karim <karimhamid@yandex.ru>
2023-04-20 23:55:57 +0300

    init empty project

---
+++ main.py
@@ -0,0 +1,18 @@
+def check_sanitizers_in_elf_file(file_path: str) -> dict[str: bool]:
+    """
+    Check if the ELF file contains sanitizers:
+    AddressSanitizer, ThreadSanitizer, MemorySanitizer.
+
+    :param file_path: path to the ELF file
+    :return: dictionary with name of sanitizer as key and bool value
+             of its presence
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
